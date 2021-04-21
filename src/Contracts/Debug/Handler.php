a17aa1941113062a572281498c7b5acea9835b44
krzysztof <krzysztof.p@devpark.pl>
2021-04-21 09:50:37 +0200

    with-logs: update to laravel 7

--- src/Contracts/Debug/Handler.php
+++ src/Contracts/Debug/Handler.php
@@ -62,9 +62,9 @@ class Handler implements ExceptionHandler
             'message' => $e->getMessage(),
             'trace' => collect($e->getTrace())->map(function ($item) {
                 return [
-                    'file' => array_get($item, 'file'),
-                    'line' => array_get($item, 'line'),
-                    'method' => array_get($item, 'function'),
+                    'file' => Arr::get($item, 'file'),
+                    'line' => Arr::get($item, 'line'),
+                    'method' => Arr::get($item, 'function'),
                 ];
             })->toArray(),
         ];
